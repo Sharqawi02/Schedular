c1bae9f235a70cbcdd654b9c589a896b105fd979
Sharqawi02 <amjad.sharqawi02@gmail.com>
2024-04-25 12:42:01 +0200

    Ändrade tabellens namn till users istället för register

--- program.py
+++ program.py
@@ -25,14 +25,14 @@ def register():
         cursor  = connection.cursor()
 
         # Check if the email already exists in the database
-        cursor.execute("""SELECT * FROM register WHERE email = %s""", (email,))
+        cursor.execute("""SELECT * FROM users WHERE email = %s""", (email,))
         user = cursor.fetchone()
         if user:
             # If the user exists, return an error message
             error = "E-postadressen är redan registrerad."
             return template('First-site.html',error=error)
 
-        cursor.execute("""INSERT INTO register (firstname, lastname, email, password)
+        cursor.execute("""INSERT INTO users (firstname, lastname, email, password)
                        VALUES(%s,%s,%s,%s)""",(firstname,lastname,email,password))
         connection.commit()
         print ("User added")
@@ -50,7 +50,7 @@ def login():
         email = request.forms.get('email')
         password = request.forms.get('password')
 
-        cursor.execute("""SELECT * FROM register WHERE email = %s AND password = %s """,(email, password))
+        cursor.execute("""SELECT * FROM users WHERE email = %s AND password = %s """,(email, password))
         user = cursor.fetchall()
 
         if user:
@@ -71,77 +71,3 @@ if __name__ == '__main__':
     run(app, debug=True)
 
 
-
-
-'''
-import psycopg2
-from bottle  import  Bottle, route, template, run, static_file, request, redirect
-from db import connect
-
-app = Bottle()
-
-@app.route('/')
-def index():
-    return template('First-Site.html')
-
-@app.route('/homepage')
-def homepage_route():
-    return template('homepage.html')
-
-# Registering a new user in the database
-@app.route('/register' , method=[ 'GET','POST'])
-def register():
-    try:
-        if request.method == 'POST':
-            firstname = request.forms.get('firstname')
-            lastname = request.forms.get('lastname')
-            email = request.forms.get('email')
-            password = request.forms.get('password')
-
-            connection = connect()
-            cursor  = connection.cursor()
-
-            cursor.execute("""INSERT INTO register (firstname, lastname, email, password)
-                           VALUES(%s,%s,%s,%s)""",(firstname,lastname,email,password))
-            connection.commit()
-            print ("User added")
-
-            return redirect('/homepage')
-        else: 
-            return template('register.html')
-    except acopg2.Error as e:
-        print("ERROR CONNECTING TO POSTGREsql:", e)
-    finally:
-        if 'cursor' in locals():
-            cursor.close()
-        if 'connection' in locals():
-            connection.close()
-
-@app.route('/login', method=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        connection = connect()
-        cursor = connection.cursor()
-        email = request.forms.get('email')
-        password = request.forms.get('password')
-
-        cursor.execute("""SELECT * FROM register WHERE email = %s AND password = %s """,(email, password))
-        user = cursor.fetchall()
-
-        if user:
-            return redirect('/homepage')
-        else:
-            error = "Ingen användare med den angivna epostadressen finns i systemet."
-            return template('First-Site.html', error=error)
-
-    return template('First-Site.html', error=error)
-
-#denna ska INTE ändras
-@app.route('/static/<filename:path>')
-def static_files(filename):
-    return static_file(filename, root='./static')
-
-if __name__ == '__main__':
-    run(app, debug=True)
-'''
